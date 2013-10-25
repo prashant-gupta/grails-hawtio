de09fa1d1024000001c1d07de251586c7e3f0afe
Prashant Gupta <prashant.gupta@middleware360.com>
2013-10-25 13:50:58 +0530

    added templates and jolokia dependency

--- hawtio/grails-app/conf/BuildConfig.groovy
+++ hawtio/grails-app/conf/BuildConfig.groovy
@@ -34,7 +34,7 @@ grails.project.dependency.resolution = {
     }
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
-
+        compile 'org.jolokia:jolokia-core:1.1.4'
         // runtime 'mysql:mysql-connector-java:5.1.20'
     }
 
